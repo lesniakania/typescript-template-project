e775966514e5ee7c3c0a9b1df8171fc47cef7b2d
Anna Ślimak <anna.slimak@emobilitydv.com>
2022-01-26 11:33:45 +0100

    Intial setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+function hello(world) {
+    return `Hello ${world}! `;
+}
+exports.hello = hello;
+console.log(hello('Ania!'));
